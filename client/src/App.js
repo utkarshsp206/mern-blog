6669b7053aa999b948fbeec1933dc455604c4bc1
utkarhsp206 <utkarhsp206@gmail.com>
2023-09-16 17:09:56 +0530

    Article Page done, next is lisitng and linking the articles

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+import Home from "./pages/Home";
+import About from "./pages/About";
+import ArticleList from "./pages/ArticleList";
+import Article from "./pages/Article";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar/>
+      <div className="max-w-screen-md mx-auto pt-20">
+        <Routes>
+          <Route path="/" element={<Home/>} />  
+          <Route path="/about" element={<About />} />  
+          <Route path="/articles-list" element={<ArticleList />} />  
+          <Route path="/article/:name" element={<Article />} />     
+        </Routes>
+      </div>
+    </Router>
+    
   );
 }
 
---
+++ client/src/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const About = () => {
+  return (
+      <div className='mb-20'>
+          <h1 className='sm:text-4xl text-2xl font-bold my-6 text-gray-900'>About</h1>
+          <p className='mx-auto leading-relaxed text-base mb-4'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia libero molestiae natus nulla! Eligendi esse corrupti, sequi dolores itaque iure, voluptate dicta, alias quasi minima distinctio voluptas nam ut est repudiandae pariatur quaerat odio quidem deserunt sapiente asperiores enim excepturi? Odit recusandae necessitatibus, commodi laborum ipsum inventore ab impedit minus.</p>
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Totam temporibus cumque impedit nemo placeat deserunt id, quasi minus ut, praesentium, numquam excepturi consequatur facilis sequi modi. Commodi vero soluta aliquam similique? Cumque, sint molestias quae, error suscipit repudiandae repellat temporibus soluta consectetur deleniti modi minus nesciunt sed? Esse, assumenda rem!</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta aliquid adipisci at placeat, ducimus dolorum eaque veritatis nemo, voluptatibus animi doloribus sapiente laborum officia quasi temporibus odio voluptatem. Magni pariatur placeat modi ipsum eveniet laboriosam fuga quod inventore. Totam illum nihil excepturi tenetur quod itaque natus eaque? Odio provident recusandae explicabo similique mollitia laudantium, possimus fugit suscipit quo animi, assumenda sapiente repellat esse natus neque, adipisci libero sunt error quasi rem. Assumenda, cum expedita sequi ipsam excepturi adipisci eligendi deleniti voluptatibus debitis velit! In, nulla molestiae. Eum eligendi laboriosam adipisci debitis cupiditate nihil ex, odio expedita perferendis ipsum, eveniet repudiandae!</p>
+      </div>
+  )
+}
+
+export default About
