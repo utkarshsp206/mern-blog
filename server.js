69a9854aa250cfb5b73f322177d068051f1d6552
utkarhsp206 <utkarhsp206@gmail.com>
2023-09-20 15:56:45 +0530

    Initialised the backend server and installed dev-dependencies

---
+++ server.js
@@ -0,0 +1,7 @@
+const express = require("express")
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+app.get("/", (req,res) => res.send("Hello, World!"));
+
+app.listen(PORT, ()=>console.log(`Server Running at port ${PORT}`))
