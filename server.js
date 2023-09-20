5fcc672534fe7f7d5632ebf6380a4a1016597fd4
utkarhsp206 <utkarshsp206@gmail.com>
2023-09-20 22:16:13 +0530

    TestCommit

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
-const express = require("express")
+const express = require("express");
+const { animationFrameScheduler } = require("rxjs");
 const app = express();
 const PORT = process.env.PORT || 8000;
 
+
+//Initialising Middleware
+//Earlier we use to install body parser but now it is a built in middleware
+// Function of Express: It parses the incoming JSON Payload
+app.use(express.json({extended: animationFrameScheduler}))
+
+
 app.get("/", (req,res) => res.send("Hello, World!"));
+app.post("/", (req,res) => res.send(`Hello ${req.body.name}`));
 
 app.listen(PORT, ()=>console.log(`Server Running at port ${PORT}`))
