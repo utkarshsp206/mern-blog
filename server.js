03ce6d82b4f5a4b4743f6251a62ecd2739127a50
utkarhsp206 <utkarshsp206@gmail.com>
2023-09-21 17:19:31 +0530

    Testing backend server

--- server.js
+++ server.js
@@ -3,11 +3,31 @@ const { animationFrameScheduler } = require("rxjs");
 const app = express();
 const PORT = process.env.PORT || 8000;
 
+const articlesInfo={
+    "learn-react":{
+        comments:[],
+    },
+    "learn-node":{
+        comments:[],
+    },
+    "my-thoughts-on-learning-react":{
+        comments:[],
+    }
+}
+
 
 //Initialising Middleware
 //Earlier we use to install body parser but now it is a built in middleware
 // Function of Express: It parses the incoming JSON Payload
 app.use(express.json({extended: animationFrameScheduler}))
+
+app.post('/api/articles/:name/add-comments', (req,res)=>{
+    const {username, text}=req.body;
+    const articlesName = req.params.name;
+    articlesInfo[articlesName].comments.push({username, text});
+    res.status(200).send(articlesInfo[articlesName])
+
+})
 
 
 app.get("/", (req,res) => res.send("Hello, World!"));
